4e6585619529103f81f2b7169062bb93e5e7b1bb
NvlFR <novalfr802@gmail.com>
2025-07-22 09:09:43 +0700

    Day 2: Completed To-Do App with Components

---
+++ vue-todo-app/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue";
+import TodoItem from "./components/TodoItem.vue";
+
+const todos = ref([
+  { id: 1, text: "Belajar Konsep Komponen", done: false },
+  { id: 2, text: "Menguasai Props & Emits", done: false },
+  { id: 3, text: "Commit Proyek ke GitHub", done: true },
+]);
+
+const red = "green";
+
+function handleDeleteTodo(idTodoDelete) {
+  todos.value = todos.value.filter((todo) => todo.id !== idTodoDelete);
+}
+</script>
+
+<template>
+  <main>
+    <h1>Aplikasi To-Do</h1>
+    <ul>
+      <TodoItem
+        v-for="item in todos"
+        :key="item.id"
+        :todo="item"
+        @delete-todo="handleDeleteTodo"
+      />
+    </ul>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 500px;
+  margin: 2rem auto;
+  font-family: sans-serif;
+}
+</style>
---
+++ vue-todo-app/src/components/TodoItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  todo: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["delete-todo"]);
+// function deleteTodo() {
+//   emit("delete-todo", todo.id);
+// }
+</script>
+
+<template>
+  <li>
+    <span>{{ todo.text }}</span>
+    <button @click="$emit('delete-todo', todo.id)">Hapus</button>
+  </li>
+</template>
+
+<style scoped>
+li {
+  padding: 0.5rem 0;
+  border-bottom: 1px solid #eee;
+}
+button {
+  margin-left: 1rem;
+}
+</style>
